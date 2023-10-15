97d92f44d5f3a7d4a7c8ec7c7dc933cf404f07f2
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-10-16 02:09:58 +0900

    add clock.css / reset.css

--- js/clock.js
+++ js/clock.js
@@ -4,8 +4,8 @@ function getClock() {
 	const date = new Date();
 	const hours = String(date.getHours()).padStart(2, "0");
 	const minutes = String(date.getMinutes()).padStart(2, "0");
-	const seconds = String(date.getSeconds()).padStart(2, "0");
-	clock.innerText = `${hours}:${minutes}:${seconds}`;
+	// const seconds = String(date.getSeconds()).padStart(2, "0");
+	clock.innerText = `${hours}:${minutes}`;
 }
 
 getClock(); //웹사이트가 로딩되자마자 한번실행
