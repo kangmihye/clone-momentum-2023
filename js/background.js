50e804ef2037435826538914012e11be8c5ef9cf
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-10-16 05:00:59 +0900

    add backgroundimg

--- js/background.js
+++ js/background.js
@@ -1,4 +1,14 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = [
+	"0.jpg",
+	"1.jpg",
+	"2.jpg",
+	"3.jpg",
+	"4.jpg",
+	"5.jpg",
+	"6.jpg",
+	"7.jpg",
+	"8.jpg",
+];
 
 const chosenImg = images[Math.floor(Math.random() * images.length)];
 
