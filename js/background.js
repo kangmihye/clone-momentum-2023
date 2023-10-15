ac8e21bf81898680ce56d99f110c85136a7f80b6
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-10-16 02:10:11 +0900

    add background.css

--- js/background.js
+++ js/background.js
@@ -2,9 +2,10 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImg = images[Math.floor(Math.random() * images.length)];
 
-const bgImg = document.createElement("img"); //태그 생성
+const bgImg = document.createElement("div"); //태그 생성
 
-bgImg.src = `img/${chosenImg}`; //프로퍼티
+bgImg.style.backgroundImage = `url(img/${chosenImg})`; //프로퍼티
+bgImg.id = "bgImg";
 
 document.body.appendChild(bgImg); // body의 가장 마지막에 element 추가
 //document.body.prepend() 맨 위에 추가
